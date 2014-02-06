a04a981b2c7e5d207f62ad652d25febc6dffcc15
Fred Sauer <fredsa@google.com>
2014-02-06 11:16:53 -0800

    new cleanup script

---
+++ scripts/cleanup-instant-tty-tarball.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+set -ue
+
+SCRIPTS_DIR=$( dirname $0 )
+ROOT_DIR=$( dirname $SCRIPTS_DIR )
+
+project=$( cat $ROOT_DIR/app.yaml | egrep '^application:' | sed 's/application: *\([0-9a-z][-0-9a-z]*[0-9a-z]\).*/\1/' )
+
+echo
+echo "Using project: $project"
+echo
+
+set -v
+gsutil rm -f gs://little-black-box/instant-tty.tar.gz
